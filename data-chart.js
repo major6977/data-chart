cb3272e8c92aec809461bf6de60e0f43c09d1cde
major davis <majordav@msn.com>
2022-03-19 17:35:45 -0700

    added htnl id nuNumbs and nuTri,styled colors and added function to move nutri from red to green

--- data-chart.js
+++ data-chart.js
@@ -1,41 +1,105 @@
-setInterval(function() {
-    function getRandomNumbers(min, max){
-         return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    let sessionsNums = document.querySelector("#sessionsNums").innerText = getRandomNumbers(2310, 2330);
-    //  let sessionsTriRed = document.querySelector("#sessionsTriRed");
-     
-     if (sessionsNums < 2324) {
-         sessionsTriRed.style.backgroundColor = "red";
-         sessionsTriRed.style.display="block";
-         sessionsTriGreen.style.display="none";
+setInterval(function () {
+  function getRandomNumbers(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  let sessionsNums = (document.querySelector("#sessionsNums").innerText =
+    getRandomNumbers(2310, 2330));
+  //  let sessionsTriRed = document.querySelector("#sessionsTriRed");
 
-
-     } else if(sessionsNums > 2324) {
-         sessionsTriGreen.style.backgroundColor = "green";
-         sessionsTriGreen.style.display="block";
-         sessionsTriRed.style.display="none";
-     }
+  if (sessionsNums < 2324) {
+    sessionsTriRed.style.backgroundColor = "red";
+    sessionsTriRed.style.display = "block";
+    sessionsTriGreen.style.display = "none";
+  } else if (sessionsNums > 2324) {
+    sessionsTriGreen.style.backgroundColor = "green";
+    sessionsTriGreen.style.display = "block";
+    sessionsTriRed.style.display = "none";
+  }
 }, 2000);
 
-            // Bounce Rate Functions
+// Bounce Rate Functions
 
- setInterval(function() {
-    function getRandomNumbers(min, max){
-        return Math.floor(Math.random() * (max - min +1)) + min;
-    }
-    let bounceRate = document.querySelector("#todayBr").innerText = getRandomNumbers(4.59, 9.10);
-        if (bounceRate < 7.99) {
-            todayBrTriRed.style.backgroundColor = "red";
-            todayBrTriRed.style.display="block";
-            todayBrTriGreen.style.display="none";
+setInterval(function () {
+  function getRandomNumbers(min, max) {
+    return Math.random() * (max - min + 1) + min;
+  }
 
-        } else if(bounceRate > 7.99) {
-            todayBrTriGreen.style.backgroundColor = "green";
-            todayBrTriGreen.style.display="block";
-            todayBrTriRed.style.display="none";
-        }
- }, 1000);    
+  let todayBrTriRed = document.querySelector("#todayBrTriRed");
+  let todayBrTriGreen = document.querySelector("#todayBrTriGreen");
+  let bounceRate = (document.querySelector("#todayBr").innerText =
+    getRandomNumbers(4, 10).toFixed(2));
+  if (bounceRate < 7) {
+    todayBrTriRed.style.backgroundColor = "red";
+    todayBrTriRed.style.display = "block";
+    todayBrTriGreen.style.display = "none";
+  } else if (bounceRate > 7) {
+    todayBrTriGreen.style.backgroundColor = "green";
+    todayBrTriGreen.style.display = "block";
+    todayBrTriRed.style.display = "none";
+  }
+}, 1000);
+
+// Movement Functions for ASD Numbers
+
+setInterval(function () {
+  function getRandomNumbers(min, max) {
+    return Math.random() * (max - min + 1) + min;
+  }
+
+  let asdTriRed = document.querySelector("#asdTriRed");
+  let asdTriGreen = document.querySelector("#asdTriGreen");
+  let asdNumbs1 = (document.querySelector("#asdNumbs1").innerText =
+    getRandomNumbers(125, 160).toFixed(1));
+
+  if (asdNumbs1 < 142) {
+      asdTriRed.style.display = "block";
+      asdTriGreen.style.display = "none";  
+  }
+  else if (asdNumbs1 > 142) {
+    asdTriGreen.style.display = "block";
+    asdTriRed.style.display = "none";  
+ }
+}, 2500);
 
 
+setInterval(function () {
+  function getRandomNumbers(min, max) {
+    return Math.random() * (max - min + 1) + min;
+  }
 
+  let asdTriRed2 = document.querySelector("#asdTriRed2");
+  let asdTriGreen2 = document.querySelector("#asdTriGreen2");
+  let asdNumbs2 = (document.querySelector("#asdNumbs2").innerText =
+    getRandomNumbers(125, 160).toFixed(1));
+
+  if (asdNumbs2 < 142) {
+      asdTriRed2.style.display = "block";
+      asdTriGreen2.style.display = "none";  
+  }
+  else if (asdNumbs2 > 142) {
+    asdTriGreen2.style.display = "block";
+    asdTriRed2.style.display = "none";  
+ }
+}, 2500);
+
+// New Users 
+
+setInterval(function () {
+  function getRandomNumbers(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  let nuTriRed = document.querySelector("#nuTriRed");
+  let nuTriGreen = document.querySelector("#nuTriGreen");
+  let nuNumbs = (document.querySelector("#nuNumbs").innerText =
+    getRandomNumbers(42,987, 60,223));
+
+  if (nuNumbs < 50,000) {
+      nuTriRed.style.display = "block";
+      nuTriGreen.style.display = "none";  
+  }
+  else if (nuNumbs > 50,000) {
+    nuTriGreen.style.display = "block";
+    nuTriRed.style.display = "none";  
+ }
+}, 3500);
